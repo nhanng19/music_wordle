8ef7429a6c29ae8e36dba097f8fe248acbdb5b7f
Daisy Lien <daisylien1211@gmail.com>
2022-08-13 16:02:22 -0700

    change

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// var playButton = $('#play');
-
-    // var textReplace = playButton.text();
-    // var whenPLay = textReplace.replace("▐▐ Play");
-    // playButton.text(whenPLay);
-
-    // $(playButton).on('click', )
-
 // lastAPI = 3fa1d67efdf9fbda6b5c07411588e640;
 // http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=3fa1d67efdf9fbda6b5c07411588e640&format=json'
 // get the top artist api 
@@ -15,24 +7,27 @@
 
 
 var playButton = document.getElementById('play');
-var trackList = document.querySelector('.track');
+var trackList = document.getElementsByClassName('track');
+var userInput = document.getElementsByClassName('guess');
+var enterBtn = document.getElementsByClassName('enter');
 
+userInput = '';
+trackList = '';
 
 playButton.addEventListener('click', function(){
-    playButton.textContent = "▐▐ Play!";
-});
+    playButton.textContent = "▐▐ Play!"
 
 fetch('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=taylorswift&limit=4&api_key=3fa1d67efdf9fbda6b5c07411588e640&format=json')
-.then(function (response){
+.then(function(response){
     return response.json();
 })
 .then(function(data){
     console.log(data);
-    for (i=0; i<=4; i++){
+    for (i=0; i<4; i++){
         var firstTrack = data['toptracks']['track'][i]['name'];
+        console.log(firstTrack);
 
         trackList[i].innerHTML = firstTrack;
     }
 })
-
-
+});
