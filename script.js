b925a64111a8a5c9bce76a6eb5f0b2e6be20c553
Jeffrey Tran <108315442+Jtran028@users.noreply.github.com>
2022-08-16 18:56:33 -0700

    JAVASCRIPT WORKFLOW

--- script.js
+++ script.js
@@ -12,6 +12,16 @@
             //     console.log("unable to get firstTrack")
             //     }
 
+// global variable for attempts
+// global variable for song data - artist infor or song info --- click play button fetch info
+
+// fetch one api at one time -- 
+
+
+var artists = ["taylorswift", "justinbieber", "theweekend", "rhianna", "michaeljackson"]
+var selectedArtists = null
+var topTracks = ['1sadfa', '1231', '12312', '234123', '12342134123']
+var userTurn = 0
 
 var playButton = $('#play');
 var trackList = $('.track');
@@ -23,30 +33,50 @@ userInput = JSON.stringify(userInput);
 
 $('#play').on('click', function(){
     $('#play').text("▐▐ Play!");
-
-fetch('https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=taylorswift&limit=4&api_key=3fa1d67efdf9fbda6b5c07411588e640&format=json')
-.then(function(response){
-    return response.json();
+    playBtn()
 })
-.then(function(data){
-    console.log(data);
-        for (i=0; i<4; i++){
-        var firstTrack = data.toptracks.track[i].name;
-        console.log(firstTrack);
-          
-        trackList[i].innerHTML = firstTrack;
-        console.log(trackList);
-        }
 
-})
-.catch(error => window.alert("Oops! Something wrong"));
+function playBtn() {
+    // grab random digt
+    var randomInt = 5
+    selectedArtists = artists[randomInt]
 
-});
+    fetch(f`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${selectedArtists}&limit=4&api_key=3fa1d67efdf9fbda6b5c07411588e640&format=json`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+            for (i=0; i<4; i++){
+            var firstTrack = data.toptracks.track[i].name;
+            console.log(firstTrack);
+            
+            trackList[i].innerHTML = firstTrack;
+            console.log(trackList);
+            
+            }
+
+    })
+    .catch(error => window.alert("Oops! Something wrong"));
+};
+
+$('#enter-button').on("click", guessBtn)
+
+function guessBtn(){
+    // var result = input == selectedArtist
+    // if match display congrats or something
+        // return
+    // if not, 
+            // if userturn+1 == 4
+                // then fetch wikipedia and display wiki to image
+                // return
+            // if userturn == 5
+                // u fail bitch
+                // return 
+            // take hint using user turn send to li, then increase turn by 1
 
 
-$('#enter-button').on("click", taylorSwiftBtn)
 
-function taylorSwiftBtn(){
     if ($('#guess').val() === "Taylor Swift"){
         theWeeknd();
     } else if ($('#guess').val() === "The Weeknd"){
@@ -80,7 +110,6 @@ function theWeeknd(){
         for (i=0; i<4; i++){
         var firstTrack = data.toptracks.track[i].name;
         console.log(firstTrack);
-          
         trackList[i].innerHTML = firstTrack;
         console.log(trackList);
         }
