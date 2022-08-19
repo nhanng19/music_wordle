fe76258dfc2ca38923e0a8ffb3991aaa75acde19
nhanng19 <nhanng19@gmail.com>
2022-08-19 14:28:12 -0700

    added animations js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,69 @@
+// Define variables for elements and user interface
 var playButton = document.getElementById("play");
 var trackList = $('.track');
 var userInput = document.getElementById("guess");
 var enterBtn = $('#enter-button');
 var feedback = document.getElementById("feedback")
-var targetDiv= document.getElementById("second");
+var targetDiv = document.getElementById("second");
 var start = document.getElementById("section");
 var restartBtn = document.getElementById("restart");
-targetDiv.style.display = "none";
-var artists = ['One Direction', 'Katy Perry', 'Justin Bieber', 'Rihanna', 'Beyonce', 'Ariana Grande', 'Drake', 
-"The Weeknd", "Eminem", "Ed Sheeran", "Taylor Swift", "Post Malone", "Bad Bunny", "Billie Eilish", "Michael Jackson", "Demi Lovato",
-"Tyga", "DaBaby", "Young Thug", "Niki", "Kehlani", "Tory Lanez", "Gunna", "Playboi Carti", "Bryson Tiller", "21 Savage", "Kendrick Lamar", 
-"Kanye West", "Travis Scott", "Lil Uzi Vert", "Future", "Meek Mill", "Lil Baby", "Kid Cudi", "Pitbull", "Lil Wayne", "Cardi B", "Jhene Aiko", "Migos",
-"Gucci Mane", "Pop Smoke", "Roddy Ricch", "XXXTENTACION", "Lil Yatchy", "6lack", "Sheck Wes"]
-var artistX = artists[Math.floor(Math.random()*artists.length)];
-console.log(artists);
-console.log(artistX);
-var userTurn = 0;
-
+var userTurn = 0; 
+var card = document.querySelector(".card")
+// Feedback audio
+ 
 var right = new Audio("assets/js/correct.wav")
 var wrong = new Audio("assets/js/error.mp3")
+var over = new Audio("assets/js/gameover.wav")
+
+// Take artists' bank and pick a random index
+
+var artists = ['One Direction', 'Katy Perry', 'Justin Bieber', 'Rihanna', 'Ariana Grande', 
+"The Weeknd", "Eminem", "Ed Sheeran", "Taylor Swift", "Post Malone", "Bad Bunny", "Billie Eilish", 
+"Michael Jackson", "Demi Lovato", "Tyga", "DaBaby", "Young Thug", "Kehlani", "Tory Lanez", 
+"Playboi Carti", "Bryson Tiller", "21 Savage", "Kendrick Lamar", "Kanye West", "Travis Scott", 
+"Lil Uzi Vert", "Meek Mill", "Lil Baby", "Kid Cudi", "Lil Wayne", "Cardi B", "Migos",
+"Gucci Mane", "Roddy Ricch", "XXXTentacion", "6lack", "Sheck Wes", "Juice Wrld", "Nelly", 
+"Nicki Minaj", "Iggy Azalea", "Ludacris", "Jay-Z", "Lil Nas X", "Wiz Khalifa", "Trey Songz", 
+"Mac Miller", "Big Sean", "ASAP Rocky", "Young Thug", "Lil Pump", "6ix9ine", "Lil Tjay", 
+"Pooh Shiesty", "Adele","Shawn Mendes", "Bruno Mars", "Shakira", "Lana Del Rey", "Justin Timberlake", 
+"Jason Derulo", "Alicia Keys", "Madonna", "Miley Cyrus", "The Beatles", "Lady Gaga", "Selena Gomez", 
+"Celine Dion", "Britney Spears", "SZA", "Mariah Carey", "Chris Brown", "Lil Durk", "Pharrell Williams",
+"50 Cent", "Kodak Black", "Chief Keef", "Jeremih"]
+var artistX = artists[Math.floor(Math.random()*artists.length)];
+
+// Load landing page and hide game 
+
+targetDiv.style.display = "none";
+
+// Loading game and hide landing page 
+
 playButton.onclick = function () {
     if (targetDiv.style.display == "none") {
       targetDiv.style.display = "block";
       start.style.display = "none";
+      document.getElementById("guess").select();
     }
   };
+
+// Fetch 4 top tracks from a random artist and appends on list
 
 fetch('https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist='+artistX+'&limit=4&api_key=3fa1d67efdf9fbda6b5c07411588e640&format=json')
 .then(function(response){
     return response.json();
 })
 .then(function(data){
-    console.log(data);
         for (i=0; i<4; i++){
         var firstTrack = data.toptracks.track[i].name;
-        console.log(firstTrack);
-          
         trackList[i].innerHTML = firstTrack;
-        console.log(trackList);
         }
+      })
 
-})
-.catch(error => window.alert("Oops! Something wrong"));
+// Check answer and provide feedback based on users' guess and turn
 
 $('#enter-button').on("click", guessBtn)
 
-
-
 function guessBtn(){
+    document.getElementById('guess').focus() 
     if ($('#guess').val().toLowerCase() === artistX.toLowerCase() || $('#guess').val().toLowerCase() === artistX.toLowerCase()){
         right.play();
         feedback.innerHTML = "Correct! It's " + artistX + "!"
@@ -57,7 +73,7 @@ function guessBtn(){
         var defaults = {
           origin: { y: 0.7 }
         };
-        
+        // confetti.js runs when user guess right
         function fire(particleRatio, opts) {
           confetti(Object.assign({}, defaults, opts, {
             particleCount: Math.floor(count * particleRatio)
@@ -89,95 +105,89 @@ function guessBtn(){
     }else{
         if(userTurn < 1){
             userTurn++
-            document.getElementById("track" + userTurn).style.display = "block";
+            document.getElementById("track" + userTurn).style.display = "block"
+            document.getElementById("track" + userTurn).style.animation = "shake 0.5s"
             feedback.innerHTML = "Not quite! Here's a hint.";
             wrong.play()
-            console.log(userTurn);
+           
         } else if (userTurn < 2){
             userTurn++
             document.getElementById("track" + userTurn).style.display = "block";
+            document.getElementById("track" + userTurn).style.animation = "shake 0.5s"
             feedback.innerHTML = "Hmm. Here's another one."
             wrong.play()
-            console.log(userTurn);
+           
         } else if (userTurn < 3){
             userTurn++
             document.getElementById("track" + userTurn).style.display = "block";
+            document.getElementById("track" + userTurn).style.animation = "shake 0.5s"
             feedback.innerHTML = "Third time's the charm!"
             wrong.play()
-            console.log(userTurn);
+           
         } else if (userTurn < 4){
             userTurn++
             document.getElementById("track" + userTurn).style.display = "block";
+            document.getElementById("track" + userTurn).style.animation = "shake 0.5s"
             feedback.innerHTML = "Look familiar?"
             wrong.play()
-            console.log(userTurn);
+       
         } else if (userTurn < 5){
-            feedback.innerHTML = "The answer was " + artistX + "!"
+            feedback.innerHTML = "Unlucky!  It's " + artistX + "!"
+            document.getElementById("track" + userTurn).style.animation = "shake 0.5s"
             restartBtn.style.display = "inline-block";
-            wrong.play()
+            over.play()
         }
     }   
-  
-    return
+  return
 };
 
-userInput.addEventListener("keydown", function (e) {
-  if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
-      guessBtn();
-  }
-});
-
-document.addEventListener("keydown", function(e){
-  if (targetDiv.style.display == "none") {
-    if (e.code === "Space") {
-      targetDiv.style.display = "block";
-      start.style.display = "none";
-    }
-}})
-
+// Fetch image from mediaWiki API 
 
 var url = "https://en.wikipedia.org/w/api.php"; 
-function removeSpace(str){
-  var strArray = str.split(" ")
-  var str2 = ""
-  for (let index = 0; index < strArray.length; index++) {
-    const element = strArray[index];
-    if (index < strArray.length - 1) {
-      str2 = str2 + element + "%20"
-    } else {
-      str2= str2 + element
-    }
-  console.log(element)
-  }
-  console.log(strArray)
-  console.log(str2)
-  return str2
-}
-
-
-
-
+var noSpace = artistX.replace(/ /g,"%20")
 var params = {
     action: "query",
     prop: "pageimages",
-    titles: `${removeSpace(artistX)}`,
+    titles: `${noSpace}`,
     format: "json",
     piprop: "original",
-
 };
-console.log(params)
 url = url + "?origin=*";
 Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-console.log(url)
 fetch(url)
     .then(function(response){return response.json();})
     .then(function(response) {
         var responseQuery = response.query.pages;
 
-        console.log(responseQuery);
-        var test1 = Object.keys(responseQuery)
-        var pages = response.query.pages[test1[0]].original.source;
-        console.log(pages);
+
+        var pageId = Object.keys(responseQuery)
+        var pages = response.query.pages[pageId[0]].original.source;
+
         document.querySelector("#img").src = pages;
-        })
-    .catch(function(error){console.log(error);});
+        });
+
+
+// Add event listener to check answer when user press enter
+
+userInput.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") { 
+      guessBtn();
+  }
+});
+
+// Add event listener to start game when user press space
+
+document.addEventListener("keyup", function(e){
+  if (targetDiv.style.display == "none") {
+    if (e.code === "Space" ) {
+      targetDiv.style.display = "block";
+      start.style.display = "none";
+      document.getElementById("guess").select();
+    }
+}})
+
+
+
+
+
+  
